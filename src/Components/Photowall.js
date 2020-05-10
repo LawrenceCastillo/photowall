5e3919812d4167baa16b7e78b795367b3e48fc43
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-05-10 19:12:34 -0400

    implemented addPhoto functionality

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Photo from './Photo'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 const Photowall = (props) => {
   return (
     <div>
-      <button className='addIcon'> + </button>
+      <Link className='addIcon' to="/AddPhoto"> </Link>
       <div className='photo-grid'>
-        {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+        {props.posts
+          .sort(function(x,y) {
+            return y.id - x.id;
+          })
+          .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
       </div>
     </div>
   )
